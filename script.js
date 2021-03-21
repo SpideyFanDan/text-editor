f05af321923d17f9691134c074959eafcad138ff
SpideyFanDan <dsmith.teach@gmail.com>
2021-03-21 14:51:13 -0400

    added bold, italic, and underline functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
  * DONE: Update the text in the "Formatted Text" section as a user types in the textarea
- * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
+ * DONE TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
  * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
  * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
  */
@@ -16,19 +16,20 @@ function updateText() {
 
 /**
  * Toggle the bold class for the output text
- * HINT: Use the onclick function insite HTML
+ * HINT: Use the onclick function inside HTML
  * HINT: Look into using this keyword
  * HINT: Use the classList property
  * HINT: Toggle .active class for the button
  */
 function makeBold(elem) {
-	//CODE GOES HERE
+	elem.classList.toggle('active');
+	document.getElementById('text-output').classList.toggle('bold');
 }
 
-/**
- * Toggle the italic class for the output text
- */
-function makeItalic(elem) {}
+function makeItalic(elem) {
+	elem.classList.toggle('active');
+	document.getElementById('text-output').classList.toggle('italic');
+}
 
 /**
  * Toggle the underline class for the output text
@@ -37,7 +38,13 @@ function makeItalic(elem) {}
  * HINT: Use contains, remove, and add functions
  */
 function makeUnderline(elem) {
-	//CODE GOES HERE
+	elem.classList.toggle('active');
+	let formattedText = document.getElementById('text-output');
+	if (formattedText.classList.contains('underline')) {
+		formattedText.classList.remove('underline');
+	} else {
+		formattedText.classList.add('underline');
+	}
 }
 
 /**
